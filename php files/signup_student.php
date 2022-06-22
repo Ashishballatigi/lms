78f9751db5a597ffa4744aef370d56c7f8e9196a
Ashishballatigi <ashishb.cs19@rvce.edu.in>
2022-06-22 16:41:12 +0530

    first commit

---
+++ php files/signup_student.php
@@ -0,0 +1,32 @@
+<?php include('header.php'); ?>
+<style>
+	body#login::before {
+    content: "";
+    background:#42c7c9;
+    position: absolute;
+    top: 0;
+    /* z-index: 1; */
+    left: 0;
+    width: 100%;
+    height: 100%;
+}
+
+
+
+</style>
+  <body id="login">
+    <div class="container">
+	<div class="row-fluid">
+	
+	<div class="span6">
+		<div class="pull-right">
+				<?php include('student_signin_form.php'); ?>
+		</div>
+	</div>
+    </div>
+	<div class="row-fluid">
+		
+
+<?php include('script.php'); ?>
+  </body>
+</html>
